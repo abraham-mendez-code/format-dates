c7b87a7d7e6511392e92dbf58fd72872bbbf8f71
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-08 09:10:19 -0400

    Added print out for GMT time in main method

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,6 +1,8 @@
 package com.pluralsight;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FormatDates {
@@ -10,14 +12,21 @@ public class FormatDates {
         // get the current local date and time
         LocalDateTime today = LocalDateTime.now();
 
+        // create a ZonedDateTime in a GMT)
+        ZonedDateTime gmtNow = ZonedDateTime.now(ZoneId.of("GMT"));
+
         // define a format for display
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MM/dd/yyyy\nyyyy-MM-dd\nMMMM dd, yyyy\nE, MMM dd yyyy HH:mm\nHH:mm 'on' dd-MMM-yyyy");
+        DateTimeFormatter fmtLocal = DateTimeFormatter.ofPattern("MM/dd/yyyy\nyyyy-MM-dd\nMMMM dd, yyyy\nE, MMM dd yyyy HH:mm\nHH:mm 'on' dd-MMM-yyyy");
 
         // apply the format to the local date time response and store it in a string
-        String todayLocal = today.format(fmt);
+        String todayLocal = today.format(fmtLocal);
 
-        // print out the formatted string for local time
-        System.out.println(todayLocal);
+        // format the ZonedDateTime using the formatter (will use the ZonedDateTime's zone)
+        String todayGMT = gmtNow.format(fmtLocal);
+
+        // print out the formatted strings for local time
+        System.out.println("Local time\n" + todayLocal + "\n");
+        System.out.println("GMT Time\n" + todayGMT + "\n");
 
     }
 
