db7f7ffb25d8019236177259e9ee84b35212411a
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-08 08:55:34 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class FormatDates {
+
+    public static void main(String[] args) {
+
+        /*
+            09/05/2021
+2021-09-05
+September 5, 2021
+Sunday, Sep 5, 2021 10:02
+         */
+
+    }
+
+}
