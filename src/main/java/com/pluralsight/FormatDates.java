582534f4281339a4d5d4c9e80e2879b8ec99c248
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-08 08:58:05 -0400

    Add print out for local time in main method

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,15 +1,23 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
 
     public static void main(String[] args) {
 
-        /*
-            09/05/2021
-2021-09-05
-September 5, 2021
-Sunday, Sep 5, 2021 10:02
-         */
+        // get the current local date and time
+        LocalDateTime today = LocalDateTime.now();
+
+        // define a format for display
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MM/dd/yyyy\nyyyy-MM-dd\nMMMM dd, yyyy\nE, MMM dd yyyy HH:mm\nHH:mm 'on' dd-MMM-yyyy");
+
+        // apply the format to the local date time response and store it in a string
+        String todayLocal = today.format(fmt);
+
+        // print out the formatted string for local time
+        System.out.println(todayLocal);
 
     }
 
